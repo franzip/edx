f2c5912c86a048cfe981006eff0ec22dbe656293
franzip <franzpezzella@gmail.com>
2014-06-25 17:37:20 +0200

    Initial commit.

---
+++ CS169.1x-sw-as-service/week4/rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -0,0 +1,39 @@
+# Add a declarative step here for populating the DB with movies.
+
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    Movie.create!(movie)
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+  end
+end
+
+# Make sure that one string (regexp) occurs before or after another one
+#   on the same page
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  assert page.body.match(/#{e1}.+#{e2}/m)
+end
+
+# Make it easier to express checking or unchecking several boxes at once
+#  "When I uncheck the following ratings: PG, G, R"
+#  "When I check the following ratings: G"
+
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  # HINT: use String#split to split up the rating_list, then
+  #   iterate over the ratings and reuse the "When I check..." or
+  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  if uncheck == "un"
+    rating_list.split(', ').each {|sel| step %{I uncheck "ratings_#{sel}"}}
+  else
+    rating_list.split(', ').each {|sel| step %{I check "ratings_#{sel}"}}
+  end
+end
+
+Then /I should see all of the movies/ do
+  # Make sure that all the movies in the app are visible in the table
+  rows = page.all("#movies tr").size - 1
+  rows.should == Movie.count
+end
